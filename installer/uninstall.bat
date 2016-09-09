f6b02f6d5c0178d773ffa27452ad3ec1f9908ca4
Phoenix1747 <aperturegds@public-files.de>
2016-09-09 14:32:31 +0200

    updated to v.1.2.

--- installer/uninstall.bat
+++ installer/uninstall.bat
@@ -5,19 +5,42 @@
 :: *-----------------------------------------------------------*
 
 TITLE Fake-Sandbox Uninstaller
-
+SET path=%~dp0
+COLOR 0F
+:: -------------------------------------------------------------------------------------------------------------------------------
 echo.
-echo If you really want to uninstall the fake-sandbox scripts, click any button to continue (close this windows otherwise)...
+echo If you really want to uninstall the fake-sandbox scripts, press any button to continue (close this windows otherwise)...
 echo.
 pause > NUL
-
+:: -------------------------------------------------------------------------------------------------------------------------------
+:: Delete the autostart file and the whole "Fake-Sandbox Processes" directory.
 del "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fake-sandbox.bat"
 rmdir /s /q "%appdata%\Fake-Sandbox Processes\"
 cls
 
+:: -------------------------------------------------------------------------------------------------------------------------------
+:: <---- delete this line and add "pause" instead to enable debugging.
+:: -------------------------------------------------------------------------------------------------------------------------------
+
+:: Look for any error:
+if errorlevel 1 goto error
+COLOR 0A
 echo.
 echo Successfully removed everything. Please relogin to make sure, there aren't any of the old processes running.
-echo They will be gone, whenever you restart anyways.
+echo They will be gone, whenever you restart anyways. Press any key to exit...
+echo.
+pause > NUL
+exit
+
+:: If there was an error, the following commands will be used:
+:error
+COLOR 0C
+echo.
+echo An error occured!
+echo The uninstaller encountered a strange and unknown error. Please enable debugging in this script - it can be found here: 
+echo %path:~0,-1%
+echo.
+echo Press any key to exit...
 echo.
 pause > NUL
 exit
