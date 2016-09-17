a0b1c8ca3ad01fee04314afffa7221b3ac0cf87c
Phoenix1747 <aperturegds@public-files.de>
2016-09-17 14:47:04 +0200

    v.1.3

--- installer/uninstall.bat
+++ installer/uninstall.bat
@@ -9,11 +9,16 @@ SET path=%~dp0
 COLOR 0F
 :: -------------------------------------------------------------------------------------------------------------------------------
 echo.
-echo If you really want to uninstall the fake-sandbox scripts, press any button to continue (close this windows otherwise)...
-echo.
-pause > NUL
+echo You are about to uninstall the fake-sandbox scripts from your computer.
+SET /P ANSWER=Are you sure you want to continue? (y/n): 
+
+if /i %ANSWER%==y goto uninstall
+if /i %ANSWER%==n goto no
+goto unrecog
+
 :: -------------------------------------------------------------------------------------------------------------------------------
 :: Delete the autostart file and the whole "Fake-Sandbox Processes" directory.
+:uninstall
 del "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fake-sandbox.bat"
 rmdir /s /q "%appdata%\Fake-Sandbox Processes\"
 cls
@@ -39,6 +44,29 @@ echo.
 echo An error occured!
 echo The uninstaller encountered a strange and unknown error. Please enable debugging in this script - it can be found here: 
 echo %path:~0,-1%
+echo.
+echo Press any key to exit...
+echo.
+pause > NUL
+exit
+
+:: -------------------------------------------------------------------------------------------------------------------------------
+:: If you chose not to uninstall this will execute:
+:no
+cls
+echo.
+echo You chose not to uninstall fake-sandbox processes. Press any key to exit...
+echo.
+pause > NUL
+exit
+
+:: If you did not choose "n" nor "y" this will appear:
+:unrecog
+COLOR 0C
+cls
+echo.
+echo An error occured!
+echo Unrecognized command. You have to choose "y" for yes and "n" for no.
 echo.
 echo Press any key to exit...
 echo.
