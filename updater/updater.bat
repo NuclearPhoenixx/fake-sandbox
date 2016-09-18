32193234437c406e3885248e509906cdb737a1a4
Phoenix1747 <Phoenix1747@users.noreply.github.com>
2016-09-18 15:09:58 +0200

    Update updater.bat

--- updater/updater.bat
+++ updater/updater.bat
@@ -1,13 +1,13 @@
 @echo off
 
 :: THIS IS THE CURRENT VERSION
-%v%=1.3
+SET v=1.3
 
 TITLE Fake-sandbox processes updater
 
 del version.txt
 start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/version', '%appdata%\Fake-Sandbox Processes\version.txt')"
-ping -n 2 localhost>NUL
+timeout /t 1>NUL
 
 if version.txt==%v% (goto ok)
 goto new
@@ -29,7 +29,7 @@ del "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fake-sandbox.bat"
 del "%appdata%\Fake-Sandbox Processes\fake-sandbox.ps1"
 start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.bat', '%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fake-sandbox.bat')"
 start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.ps1', '%appdata%\Fake-Sandbox Processes\fake-sandbox.ps1')"
-ping -n 3 localhost>NUL
+timeout /t 3>NUL
 cls
 COLOR 0A
 echo.
