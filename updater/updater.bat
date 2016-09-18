239bfc46f05c59d855280780f36aff60c3cc3009
Phoenix1747 <Phoenix1747@users.noreply.github.com>
2016-09-18 14:20:18 +0200

    Update updater.bat

--- updater/updater.bat
+++ updater/updater.bat
@@ -2,7 +2,7 @@
 TITLE Fake-sandbox processes updater
 
 
-start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/version', 'C:\Users\Matthias\AppData\Roaming\Fake-Sandbox Processes\version.txt')"
+start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/version', '%appdata%\Fake-Sandbox Processes\version.txt')"
 ping -n 2 localhost>NUL
 
 if version.txt == 1.3 (goto ok)
@@ -16,8 +16,8 @@ if /i %ANSWER%==n (goto no)
 goto unrecog
 
 :install
-start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.bat', 'C:\Users\Matthias\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\fake-sandbox.bat')"
-start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.ps1', 'C:\Users\Matthias\AppData\Roaming\Fake-Sandbox Processes\fake-sandbox.ps1')"
+start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.bat', '%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fake-sandbox.bat')"
+start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.ps1', '%appdata%\Fake-Sandbox Processes\fake-sandbox.ps1')"
 exit
 
 :ok
