132f02a7620ef8b2fdfce3e50273b8fef7c64d9f
Phoenix1747 <aperturegds@public-files.de>
2016-09-18 17:24:47 +0200

    fix 5

--- updater/updater.bat
+++ updater/updater.bat
@@ -7,9 +7,6 @@ SET /p v=<"%appdata%\Fake-SandboxProcesses\current_version.txt"
 TITLE Fake-sandbox processes updater
 
 ::-------------------------------------------------------------------
-:: Delete old version.txt
-del "%appdata%\Fake-SandboxProcesses\current_version.txt"
-cls
 :: Download new version.txt
 start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/version', '%appdata%\Fake-SandboxProcesses\version.txt')"
 ping -n 2 127.0.0.1>NUL
@@ -22,7 +19,7 @@ goto new
 :: Ask to install the new version
 :new
 SET /p version=<"%appdata%\Fake-SandboxProcesses\version.txt"
-del "%appdata%\Fake-SandboxProcesses\current_version.txt"
+del "%appdata%\Fake-SandboxProcesses\version.txt"
 msg * A new version (%version%) of Fake Sandbox Processes is available!
 echo.
 SET /P ANSWER=Would you like to install the update? (y/n): 
@@ -40,6 +37,8 @@ del "%appdata%\Fake-SandboxProcesses\fake-sandbox.ps1"
 start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.bat', '%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\fake-sandbox.bat')"
 start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.ps1', '%appdata%\Fake-SandboxProcesses\fake-sandbox.ps1')"
 ping -n 3 127.0.0.1>NUL
+del "%appdata%\Fake-SandboxProcesses\current_version.txt"
+echo %version%>>"%appdata%\Fake-SandboxProcesses\current_version.txt"
 cls
 COLOR 0A
 echo.
