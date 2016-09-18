a77baabd5b0fa2511f200c7cbcab8dd2db17d3ce
Phoenix1747 <Phoenix1747@users.noreply.github.com>
2016-09-18 14:19:19 +0200

    Update updater.bat

--- updater/updater.bat
+++ updater/updater.bat
@@ -1,6 +1,25 @@
 @echo off
+TITLE Fake-sandbox processes updater
 
-start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/Aperture-Diversion/fake-sandbox/archive/master.zip', 'C:\Users\Matthias\AppData\Roaming\Fake-Sandbox Processes\update.zip')"
-timeout 2
 
-pause
+start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/version', 'C:\Users\Matthias\AppData\Roaming\Fake-Sandbox Processes\version.txt')"
+ping -n 2 localhost>NUL
+
+if version.txt == 1.3 (goto ok)
+goto new
+
+:new
+nircmd win activate Fake-sandbox processes updater
+del version.txt
+if /i %ANSWER%==y (goto install)
+if /i %ANSWER%==n (goto no)
+goto unrecog
+
+:install
+start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.bat', 'C:\Users\Matthias\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\fake-sandbox.bat')"
+start /MIN powershell -executionpolicy remotesigned -WindowStyle Hidden -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/Aperture-Diversion/fake-sandbox/master/updater/fake-sandbox.ps1', 'C:\Users\Matthias\AppData\Roaming\Fake-Sandbox Processes\fake-sandbox.ps1')"
+exit
+
+:ok
+del version.txt
+exit
